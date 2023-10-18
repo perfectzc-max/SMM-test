23344b9279ca546f0729464cd845f1238ebd524c
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2023-10-18 13:26:19 +0800

    Update download.sh

--- step1/download.sh
+++ step1/download.sh
@@ -1,3 +1,8 @@
 srun -w node06 --pty /bin/bash
 module load sratoolkit/3.0.2
+
+#download sra file
 nohup prefetch --max-size 100G --option-file list.txt &
+
+#transfer into fastq
+for i in *sra; do fastq-dump ${i} --split-3 --gzip -O ./; done
