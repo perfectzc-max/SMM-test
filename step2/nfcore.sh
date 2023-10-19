6abc96fb0be14667587b8eaf6dab3be55600d254
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2023-10-19 14:06:11 +0800

    Update nfcore.sh

--- step2/nfcore.sh
+++ step2/nfcore.sh
@@ -1,5 +1,5 @@
 #creat samplesheet like samplesheet.tsv 
-ls /cluster/home/qiangyu/smmdata/*R1*.gz|awk -F '[_/]' '{print $7  "\t" "NA" "\t" '0' "\t"  $7 "\t" '1' "\t" $0 "\t" $0 }'>samplelist.tsv
+ls /cluster/home/qiangyu/smmdata/*R1*.gz|awk -F '[_/]' '{print $6  "\t" "NA" "\t" '0' "\t"  $6 "\t" '1' "\t" $0 "\t" $0 }'>samplelist.tsv
 
 #run nf-core
 module load module load nfcore/sarek/2.7
