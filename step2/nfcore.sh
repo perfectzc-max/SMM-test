4ad8a10ab6361a6fcf428173acebf33e9339095c
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2023-10-20 13:13:01 +0800

    Update nfcore.sh

--- step2/nfcore.sh
+++ step2/nfcore.sh
@@ -1,6 +1,7 @@
 #creat samplesheet like samplesheet.tsv 
 ls /cluster/home/qiangyu/smmdata/*_1*.gz|awk -F '[_/]' '{print $6  "\t" "NA" "\t" '0' "\t"  $6 "\t" '1' "\t" $0 "\t" $0 }'>samplelist.tsv
-
+#vi change the filename
+vi samplelist.tsv
 #run nf-core
 module load module load nfcore/sarek/2.7
 run_nfcore_haplotypecaller_vep.sh GRCh38 /cluster/home/qiangyu/smmdata/samplelist.tsv /cluster/home/qiangyu/bulkdata/nfcore/
