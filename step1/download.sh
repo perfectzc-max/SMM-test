6432527cd8989624dae87e3e429f3b8b21dec0af
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2023-10-24 15:19:05 +0800

    Update download.sh

--- step1/download.sh
+++ step1/download.sh
@@ -10,3 +10,8 @@ awk '{print "nohup prefetch --max-size 100G " $0 " &"}' list.txt|bash
 for i in *sra; do fastq-dump ${i} --split-3 --gzip -O ./; done
 #OR 
 awk '{print "nohup fastq-dump --split-3 --gzip /cluster/home/qiangyu/smmdata/" $1 "/" $1 ".sra -O ./ &"}' list.txt |bash
+
+#submit sbatch job
+awk '{print "nohup fastq-dump --split-3 --gzip /cluster/home/qiangyu/smmdata/" $1 "/" $1 ".sra -O ./ &"}' list.txt> code.txt
+cat sbach.head code.sh >job.slurm
+sbatch job.slurm
