a7d1985b562c125f8f1373b08943916a789335ed
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2023-10-28 16:48:29 +0800

    Update smm_gatk.sh

--- step2/smm_gatk.sh
+++ step2/smm_gatk.sh
@@ -9,9 +9,10 @@ SAMPLE_ID=$(echo "$INPUT_R1" | sed 's/_R1.*//')
 export GATK_PATH=/path/to/gatk
 export TRIM_GALORE_PATH=/path/to/trim_galore
 export FASTQC_PATH=/path/to/fastqc
+
+#reference dir
 export REFERENCE=reference.fasta
 export DBSNP=dbsnp.vcf
-export TARGET_BED=chrY_9055175-9057608.bed
 export KNOWN_INDELS=Homo_sapiens_assembly38.known_indels.vcf.gz
 export MILLS_GOLD_INDELS=Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
 
@@ -45,10 +46,10 @@ else
 fi
 
 # Base quality score recalibration (BQSR)
-$GATK_PATH BaseRecalibrator -R $REFERENCE -I "$SAMPLE_ID.md.bam" -O "$SAMPLE_ID.recal_data.table" --known-sites $DBSNP --known-sites $KNOWN_INDELS --known-sites $MILLS_GOLD_INDELS -L $TARGET_BED --verbosity INFO
+$GATK_PATH BaseRecalibrator -R $REFERENCE -I "$SAMPLE_ID.md.bam" -O "$SAMPLE_ID.recal_data.table" --known-sites $DBSNP --known-sites $KNOWN_INDELS --known-sites $MILLS_GOLD_INDELS --verbosity INFO
 
 # Apply BQSR, including optimized Java virtual machine options
-$GATK_PATH ApplyBQSR -R $REFERENCE --input "$SAMPLE_ID.md.bam" --output "$SAMPLE_ID.recalibrated.bam" -L $TARGET_BED --bqsr-recal-file "$SAMPLE_ID.recal_data.table"
+$GATK_PATH ApplyBQSR -R $REFERENCE --input "$SAMPLE_ID.md.bam" --output "$SAMPLE_ID.recalibrated.bam"  --bqsr-recal-file "$SAMPLE_ID.recal_data.table"
 
 # Variant calling (HaplotypeCaller)
 $GATK_PATH HaplotypeCaller -R $REFERENCE -I "$SAMPLE_ID.recalibrated.bam" -O "$SAMPLE_ID.g.vcf"
@@ -57,7 +58,7 @@ $GATK_PATH HaplotypeCaller -R $REFERENCE -I "$SAMPLE_ID.recalibrated.bam" -O "$S
 $GATK_PATH IndexFeatureFile -I "$SAMPLE_ID.g.vcf"
 
 # Genotype gVCF files to create a VCF file
-$GATK_PATH GenotypeGVCFs -R $REFERENCE -L $TARGET_BED --D $DBSNP -V "$SAMPLE_ID.g.vcf" -O "$SAMPLE_ID.vcf"
+$GATK_PATH GenotypeGVCFs -R $REFERENCE --D $DBSNP -V "$SAMPLE_ID.g.vcf" -O "$SAMPLE_ID.vcf"
 
 # Merge variant records
 $GATK_PATH GatherVcfs -I "$SAMPLE_ID.vcf" -O merged_variants.vcf
