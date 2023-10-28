b95ddaf1e4e182e8dbf7e4bc1f2504cdbe03735a
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2023-10-28 15:55:27 +0800

    Update self_pipeline.sh

--- step2/self_pipeline.sh
+++ step2/self_pipeline.sh
@@ -1,1 +1,44 @@
+#!/bin/bash
+
+# 设置环境变量
+export GATK_PATH=/path/to/gatk
+export TRIM_GALORE_PATH=/path/to/trim_galore
+export FASTQC_PATH=/path/to/fastqc
+export REFERENCE=reference.fasta
+export DBSNP=dbsnp.vcf
+
+# 提取样本ID
+INPUT_R1="id_R1.fastq.gz"
+INPUT_R2="id_R2.fastq.gz"
+SAMPLE_ID=$(echo "$INPUT_R1" | sed 's/_R1.*//')
+
+# 运行FastQC以进行质量评估
+$FASTQC_PATH -o fastqc_reports "$INPUT_R1" "$INPUT_R2"
+
+# 运行Trim Galore以去除低质量的碱基和接头
+$TRIM_GALORE_PATH --paired --output_dir trimmed_data "$INPUT_R1" "$INPUT_R2"
+
+# 数据预处理：Fastq到BAM格式的映射，包括带有样本ID的bwa mem命令
+bwa mem -K 100000000 -R "@RG\tID:1\tPU:1\tSM:$SAMPLE_ID\tLB:$SAMPLE_ID\tPL:illumina" -t 16 -M $REFERENCE trimmed_data/"${SAMPLE_ID}_R1_val_1.fq" trimmed_data/"${SAMPLE_ID}_R2_val_2.fq" | samtools view -bS - > mapped.bam
+
+# 标记重复序列
+$GATK_PATH MarkDuplicates -I mapped.bam -O deduplicated.bam -M metrics.txt
+
+# 基本质量分数校正（BQSR）
+$GATK_PATH BaseRecalibrator -R $REFERENCE -I deduplicated.bam --known-sites $DBSNP -O recal_data.table
+
+# 应用基本质量分数校正（BQSR）
+$GATK_PATH ApplyBQSR -R $REFERENCE -I deduplicated.bam --bqsr-recal-file recal_data.table -O recalibrated.bam
+
+# 变异调用（HaplotypeCaller）
+$GATK_PATH HaplotypeCaller -R $REFERENCE -I recalibrated.bam -O "$SAMPLE_ID.vcf"
+
+# 合并变异记录
+$GATK_PATH GatherVcfs -I "$SAMPLE_ID.vcf" -O merged_variants.vcf
+
+# 过滤和注释变异
+# 这一步通常需要使用额外的工具来过滤和注释变异，如GATK的VariantFiltration，Annovar，或其他工具，具体根据您的需求选择。
+
+# 结果报告
+# 生成包含变异信息的结果报告，通常以VCF格式。此处根据需求和工具自定义。
 
