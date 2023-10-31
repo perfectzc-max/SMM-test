b6e98353462dcc9fce9d9fbc6ee031633040de8b
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2023-10-31 16:26:40 +0800

    Update run_smm_gatk.sh

--- step2/run_smm_gatk.sh
+++ step2/run_smm_gatk.sh
@@ -1,4 +1,5 @@
 #example command
-sbatch gatkpip.batch F /cluster/home/qiangyu/smmdata/SRR15669406_1.fastq.gz /cluster/home/qiangyu/smmdata/SRR15669406_2.fastq.gz SRR15669406 /cluster/groups/Jan-Lab/qiangyu/smmpipe/SRR15669406
+#sbatch smm_gatk.batch <is bulk?> <fq1> <fq2> <id> <outdir>
+sbatch smm_gatk.batch F /cluster/home/qiangyu/smmdata/SRR15669406_1.fastq.gz /cluster/home/qiangyu/smmdata/SRR15669406_2.fastq.gz SRR15669406 /cluster/groups/Jan-Lab/qiangyu/smmpipe/SRR15669406
 
-#
+
