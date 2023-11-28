da950d25235df8a2866d3830cfae46e2fd0fe304
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2023-11-28 14:29:02 +0800

    Create umi_qc.sh

---
+++ step3/umi_qc.sh
@@ -0,0 +1,7 @@
+### 1.umi count for S-lib
+umi_extractor.bash S-file.bam 1
+python SMM_qc.py umi S-file.1.bam.umi
+
+### 2.pileup sum for G_lib
+SMM_pileup.bash G-file.bam
+python SMM_qc.py plp G-file.mpu
