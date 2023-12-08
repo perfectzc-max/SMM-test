52305457240d67c77342b9d4a1b14083d05a137c
perfectzc-max <67649301+perfectzc-max@users.noreply.github.com>
2023-12-08 14:53:43 +0800

    Update umi_qc.sh

--- step3/umi_qc.sh
+++ step3/umi_qc.sh
@@ -3,5 +3,5 @@ umi_extractor.bash S-file.bam 1
 python SMM_qc.py umi S-file.1.bam.umi
 
 ### 2.pileup sum for G_lib
-sbatch SMM_pileup.bash G-file.bam
+sbatch SMM_pileup.bash bamdir G-file.bam
 python SMM_qc.py plp G-file.mpu
